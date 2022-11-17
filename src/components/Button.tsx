b87261c17d38caccee1ae8f82f2e32f8e3572c1e
diogenespasqualoto <diogenespasqualoto147@gmail.com>
2022-11-17 07:23:49 -0300

    implementing DarkMode

---
+++ src/components/Button.tsx
@@ -0,0 +1,17 @@
+import { useContext } from "react";
+import { TouchableOpacity, Text } from "react-native";
+import { ThemeContext } from "../context/ThemeContext";
+import { Styles } from "../styles/GlobalStyles";
+
+interface ButtonProps {
+  onPress: () => void;
+  title: string;
+  isBlue?: boolean;
+  isGray?: boolean;
+}
+
+export default function Button({ title, onPress, isBlue, isGray }: ButtonProps) {
+
+    
+
+}
---
+++ src/styles/GlobalStyles.tsx
@@ -0,0 +1,6 @@
+import { StyleSheet } from "react-native";
+import { myColors } from "./Colors";
+
+export const Styles = StyleSheet.create({
+  btn: {},
+});
